f2914a10e15ac2282e28bf1c64844982daaf164f
Priyanshi Mittal <112064454+priyanshi1404@users.noreply.github.com>
2025-05-18 11:56:56 +0530

    Time: 133 ms (44.44%) | Memory: 44.5 MB (56.79%) - LeetSync

--- 2061-painting-a-grid-with-three-different-colors/painting-a-grid-with-three-different-colors.java
+++ 2061-painting-a-grid-with-three-different-colors/painting-a-grid-with-three-different-colors.java
@@ -2,59 +2,63 @@ class Solution {
     static final int MOD = 1_000_000_007;
 
     public int colorTheGrid(int m, int n) {
-        List<int[]> states = new ArrayList<>();
-        generateStates(new int[m], 0, states);
+        List<Integer> validStates = new ArrayList<>();
+        Map<Integer, List<Integer>> compatible = new HashMap<>();
 
-        Map<String, List<String>> compatible = new HashMap<>();
-        for (int[] s1 : states) {
-            String key1 = Arrays.toString(s1);
-            compatible.putIfAbsent(key1, new ArrayList<>());
-            for (int[] s2 : states) {
-                if (isCompatible(s1, s2)) {
-                    compatible.get(key1).add(Arrays.toString(s2));
+        generateValidStates(0, -1, m, 0, validStates);
+
+        for (int a : validStates) {
+            for (int b : validStates) {
+                if (isCompatible(a, b, m)) {
+                    compatible.computeIfAbsent(a, x -> new ArrayList<>()).add(b);
                 }
             }
         }
 
-        Map<String, Integer> dp = new HashMap<>();
-        for (int[] state : states) {
-            dp.put(Arrays.toString(state), 1);
+        Map<Integer, Integer> dp = new HashMap<>();
+        for (int state : validStates) {
+            dp.put(state, 1);
         }
 
-        for (int col = 1; col < n; col++) {
-            Map<String, Integer> newDp = new HashMap<>();
-            for (String prev : dp.keySet()) {
+        for (int i = 1; i < n; i++) {
+            Map<Integer, Integer> newDp = new HashMap<>();
+            for (int prev : dp.keySet()) {
                 int count = dp.get(prev);
-                for (String next : compatible.get(prev)) {
+                for (int next : compatible.get(prev)) {
                     newDp.put(next, (newDp.getOrDefault(next, 0) + count) % MOD);
                 }
             }
             dp = newDp;
         }
 
-        int res = 0;
+        int result = 0;
         for (int val : dp.values()) {
-            res = (res + val) % MOD;
+            result = (result + val) % MOD;
         }
-        return res;
+
+        return result;
     }
 
-    private void generateStates(int[] state, int index, List<int[]> states) {
-        if (index == state.length) {
-            states.add(Arrays.copyOf(state, state.length));
+    private void generateValidStates(int pos, int prevColor, int m, int currState, List<Integer> states) {
+        if (pos == m) {
+            states.add(currState);
             return;
         }
+
         for (int color = 0; color < 3; color++) {
-            if (index == 0 || state[index - 1] != color) {
-                state[index] = color;
-                generateStates(state, index + 1, states);
+            if (color != prevColor) {
+                generateValidStates(pos + 1, color, m, currState * 3 + color, states);
             }
         }
     }
 
-    private boolean isCompatible(int[] s1, int[] s2) {
-        for (int i = 0; i < s1.length; i++) {
-            if (s1[i] == s2[i]) return false;
+    private boolean isCompatible(int a, int b, int m) {
+        for (int i = 0; i < m; i++) {
+            int ca = a % 3;
+            int cb = b % 3;
+            if (ca == cb) return false;
+            a /= 3;
+            b /= 3;
         }
         return true;
     }
