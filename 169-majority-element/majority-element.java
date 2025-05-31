4b65979e2209b095ac6f20a7cade2a6ab23a3721
Priyanshi Mittal <112064454+priyanshi1404@users.noreply.github.com>
2025-05-31 16:29:37 +0530

    Time: 2 ms (75.00%) | Memory: 53.2 MB (39.14%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,17 +1,48 @@
 class Solution {
+    //Approach 3-Boyer-Moore voting Algo
     public int majorityElement(int[] nums) {
-        int n=nums.length;
-        for(int i=0;i<nums.length;i++){
-            int count=0;
-            for(int j=0;j<nums.length;j++){
-                if(nums[i] == nums[j]){
-                    count++;
-                }
+        int count=0;
+        int majElement=0;
+        for(int num:nums){
+            if(count==0){
+                majElement=num;
             }
-            if(count>n/2){
-                return nums[i];
+            if(num==majElement){
+                count++;
+            }
+            else{
+                count--;
             }
         }
-        return -1;
+        return majElement;
     }
+    //Approach 2-Hashing
+    // public int majorityElement(int[] nums) {
+    //     int n=nums.length;
+    //     HashMap<Integer,Integer> map=new HashMap<>();
+    //     for(int i=0;i<nums.length;i++){
+    //         map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+    //         if(map.get(nums[i])>n/2){
+    //             return nums[i];
+    //         }
+    //     }
+    //     return -1;
+    // }
+    
+    //Approach 1-BRUTE FORCE
+    // public int majorityElement(int[] nums) {
+    //     int n=nums.length;
+    //     for(int i=0;i<nums.length;i++){
+    //         int count=0;
+    //         for(int j=0;j<nums.length;j++){
+    //             if(nums[i] == nums[j]){
+    //                 count++;
+    //             }
+    //         }
+    //         if(count>n/2){
+    //             return nums[i];
+    //         }
+    //     }
+    //     return -1;
+    // }
 }
