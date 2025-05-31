7ec5cd6bf86eb5ab7ebb34753019e7306435f3ad
Priyanshi Mittal <112064454+priyanshi1404@users.noreply.github.com>
2025-05-31 16:49:28 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (40.11%) - LeetSync

--- 1603-running-sum-of-1d-array/running-sum-of-1d-array.java
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.java
@@ -1,12 +1,21 @@
 class Solution {
-    //Approach 1-BRUTE FORCE
     public int[] runningSum(int[] nums) {
-        int prefix[]=new int[nums.length];
-        prefix[0]=nums[0];
+       
         for(int i=1;i<nums.length;i++){
-            prefix[i]=prefix[i-1]+nums[i];
+           nums[i]=nums[i-1]+nums[i];
         }
-        return prefix;
+        return nums;
         
     }
+
+    //Approach 1-BRUTE FORCE TC(O(N))
+    // public int[] runningSum(int[] nums) {
+    //     int prefix[]=new int[nums.length];
+    //     prefix[0]=nums[0];
+    //     for(int i=1;i<nums.length;i++){
+    //         prefix[i]=prefix[i-1]+nums[i];
+    //     }
+    //     return prefix;
+        
+    // }
 }
