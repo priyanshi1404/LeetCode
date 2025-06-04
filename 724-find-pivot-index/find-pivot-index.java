568dd4315b8d000f47f0b99fc0b829ed92de4c2b
Priyanshi Mittal <112064454+priyanshi1404@users.noreply.github.com>
2025-06-04 15:09:58 +0530

    Time: 1 ms (93.08%) | Memory: 45.1 MB (97.87%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.java
+++ 724-find-pivot-index/find-pivot-index.java
@@ -1,6 +1,25 @@
 class Solution {
-    //Approach 01-BRUTE FORCE 
-    public int pivotIndex(int[] nums) {
+    //Approach 02- PRE-COMPUTATION
+    public int pivotIndex(int[] nums){
+        //pre-compute sum
+        int sum=0;
+        for(int i:nums){
+            sum=sum+i;
+        }
+        int left=0;
+        //i have total sum
+        for(int i=0;i<nums.length;i++){
+            int right=sum-left-nums[i];
+            if(left==right){
+                return i;
+            }
+            left=left+nums[i];
+        }
+        return -1;
+    }
+
+    //Approach 01-BRUTE FORCE TC-O(n^2)
+    public int pivotIndexOld(int[] nums) {
         for(int i=0;i<nums.length;i++){
             int leftSum=0;
             for(int j=0;j<i;j++){
