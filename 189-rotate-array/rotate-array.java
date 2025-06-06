fa12cd935dc426be7e6ccaca6024369653e9103e
Priyanshi Mittal <112064454+priyanshi1404@users.noreply.github.com>
2025-06-07 02:15:36 +0530

    Time: 1 ms (47.15%) | Memory: 57.7 MB (22.20%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,5 +1,5 @@
 class Solution {
-    //approach 02
+    //approach 02- Using an extra array 
     public void rotate(int[] nums, int k){
         int n=nums.length;
         k=k%n;
@@ -12,16 +12,16 @@ class Solution {
         }
     }
     // approach 01- BRUTE FORCE  TC(O(N^k)) TLE
-    public void rotateOld(int[] nums, int k) {
-        int n=nums.length;
-        k=k%n;  //it keeps the array in bounds
+    // public void rotateOld(int[] nums, int k) {
+    //     int n=nums.length;
+    //     k=k%n;  //it keeps the array in bounds
 
-        for(int i=0;i<k;i++){
-            int last=nums[n-1];  //saving because after one rotation the last element will be deleted hence we need to save it
-            for(int j=n-1;j>0;j--){
-                nums[j]=nums[j-1];
-            }
-            nums[0]=last;
-        }
-    }
+    //     for(int i=0;i<k;i++){
+    //         int last=nums[n-1];  //saving because after one rotation the last element will be deleted hence we need to save it
+    //         for(int j=n-1;j>0;j--){
+    //             nums[j]=nums[j-1];
+    //         }
+    //         nums[0]=last;
+    //     }
+    // }
 }
