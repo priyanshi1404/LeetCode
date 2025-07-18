3aea40af25494e5b3f10cd5aadb05d7700fd66bc
Priyanshi Mittal <mittalpriyanshi1457@gmail.com>
2025-07-19 01:57:05 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (29.44%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -13,19 +13,50 @@
  *     }
  * }
  */
+class Pair{
+    TreeNode p;
+    TreeNode q;
+    Pair(TreeNode p,TreeNode q){
+        this.p=p;
+        this.q=q;
+    }
+}
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p==null && q==null){
-            return true;
+        //Iterative Approach
+        Stack<Pair> stack = new Stack<>();
+        stack.push(new Pair(p,q));
+        while(!stack.isEmpty()){
+            Pair pair=stack.pop();
+            TreeNode node1=pair.p;
+            TreeNode node2=pair.q;
+            if(node1==null && node2==null){
+                continue;
+            }
+            if(node1==null || node2==null){
+                return false;
+            }
+            if(node1.val != node2.val){
+                return false;
+            }
+            stack.push(new Pair(node1.left,node2.left));
+            stack.push(new Pair(node1.right,node2.right));
         }
-        if(p==null || q==null){
-            return false;
-        }
-        if(p.val!=q.val){
-            return false;
-        }
-        boolean  leftCompare= isSameTree(p.left,q.left);
-        boolean rightCompare= isSameTree(p.right,q.right);
-        return  leftCompare &&  rightCompare;
+        return true;
+        
+
+        
+        // if(p==null && q==null){
+        //     return true;
+        // }
+        // if(p==null || q==null){
+        //     return false;
+        // }
+        // if(p.val!=q.val){
+        //     return false;
+        // }
+        // boolean  leftCompare= isSameTree(p.left,q.left);
+        // boolean rightCompare= isSameTree(p.right,q.right);
+        // return  leftCompare &&  rightCompare;
     }
 }
