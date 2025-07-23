a236e866cf87fcc3071661b5a374e369cc7da775
Priyanshi Mittal <mittalpriyanshi1457@gmail.com>
2025-07-23 12:32:05 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (69.98%) - LeetSync

--- 476-number-complement/number-complement.java
+++ 476-number-complement/number-complement.java
@@ -1,5 +1,8 @@
 class Solution {
     public int findComplement(int num) {
+        if(num==0){
+            return 1;
+        }
         //step 01-count length of bits
         int numberOfBits=Integer.toBinaryString(num).length();
         int mask=(1<< numberOfBits) - 1;
