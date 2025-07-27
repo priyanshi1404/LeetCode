7e0a6f66a57fa816548592b75f78a3c833b5fd9c
Priyanshi Mittal <mittalpriyanshi1457@gmail.com>
2025-07-27 20:53:24 +0530

    Time: 1 ms (33.40%) | Memory: 41.7 MB (41.19%) - LeetSync

---
+++ 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int countHillValley(int[] nums) {
+        List<Integer> filtered = new ArrayList<>();
+        filtered.add(nums[0]);
+
+        // Step 1: Remove consecutive duplicates
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                filtered.add(nums[i]);
+            }
+        }
+
+        // Step 2: Count hills and valleys
+        int count = 0;
+        for (int i = 1; i < filtered.size() - 1; i++) {
+            int prev = filtered.get(i - 1);
+            int curr = filtered.get(i);
+            int next = filtered.get(i + 1);
+
+            if ((curr > prev && curr > next) || (curr < prev && curr < next)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
